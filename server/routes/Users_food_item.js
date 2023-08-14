fca226cd8167eb72297be396dd699f41afd87c66
Mark Gayler <mark.gayler.2022@uni.strath.ac.uk>
2023-08-14 04:47:57 +0100

    add some api docs

--- server/routes/Users_food_item.js
+++ server/routes/Users_food_item.js
@@ -1,5 +1,3 @@
-
-
 const express = require("express");
 const router = express.Router();
 const { users_food_item, global_food_item } = require("../models");
@@ -35,6 +33,44 @@ router.post("/", async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
+/**
+ * @api {get} /userfooditems/byId/:id Get Individual Food Item
+ * @apiName GetIndividualFoodItem
+ * @apiGroup Users_Food_Item
+ *
+ * @apiParam {Number} id Food item's unique ID.
+ *
+ *
+ * @apiSuccess {Object} item Individual food item.
+ * @apiSuccess {Number} item.id Item ID.
+ * @apiSuccess {String} item.name Name of the food item.
+ * @apiSuccess {Number} item.energy energy per 100g.
+ * @apiSuccess {Number} item.protein Protein per 100g
+ * @apiSuccess {Number} item.fibre Fibre per 100g
+ * @apiSuccess {String} item.image image
+ *
+ * @apiSuccessExample {json} Success Response
+ * HTTP/1.1 200 OK
+ * {
+ *"id": 3,
+ *"user": 1,
+ *"food_name": "potato",
+ *"energy": 10,
+ *"protien": 0.5,
+ *"fibre": 1.1,
+ *"image": "onion"
+ *}
+ *
+ * @apiErrorExample {json} Error Response
+ * HTTP/1.1 500 Internal Server Error
+ * {
+ *   "error": "Internal Server Error"
+ * }
+ *
+ * @apiDescription This endpoint retrieves an individual food item by its unique ID.
+ * The food item's ID is obtained from the request URL parameters.
+ * The response includes information such as item ID, name, enerhy, protein, fibre, and the image.
+ */
 
 // GET(byID) get individual item, by using the primary key, id, from the url
 router.get("/byId/:id", async (req, res) => {
@@ -91,9 +127,8 @@ router.delete("/byId/:id", async (req, res) => {
   }
 });
 
-//GET BOTH USER and GLOBAL items  
+//GET BOTH USER and GLOBAL items
 // Each user will receive their personal user items and all global food items
-
 
 router.get("/all", async (req, res) => {
   try {
@@ -119,8 +154,5 @@ router.get("/all", async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
-
-
-
 
 module.exports = router;
--- server/routes/Water.js
+++ server/routes/Water.js
@@ -2,6 +2,31 @@ const express = require("express");
 const router = express.Router();
 const { water_tracking } = require("../models");
 
+/**
+ * @api {get} /water Get Today's Water Intake
+ * @apiName GetTodaysWaterIntake
+ * @apiGroup Water
+ *
+ * @apiHeader {Number} userId User's unique ID .
+ *
+ * @apiSuccess {Number} todaysIntake Water intake for the current date (in milliliters).
+ *
+ * @apiSuccessExample {json} Success Response
+ * HTTP/1.1 200 OK
+ * {
+ *   "todaysIntake": 1500
+ * }
+ *
+ * @apiErrorExample {json} Error Response
+ * HTTP/1.1 500 Internal Server Error
+ * {
+ *   "error": "Internal Server Error"
+ * }
+ *
+ * @apiDescription This endpoint retrieves the water intake record for the authenticated user on the current date.
+ * The user's unique ID is obtained from the request header. The endpoint returns the amount of water intake in milliliters.
+ * If no intake record is found for the user on the current date, the response will have an intake value of 0.
+ */
 // get today's water consumption
 router.get("/", async (req, res) => {
   try {
@@ -28,7 +53,31 @@ router.get("/", async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 });
-
+/**
+ * @api {post} /water Add or Update Water Intake
+ * @apiName AddOrUpdateWaterIntake
+ * @apiGroup Water
+ *
+ * @apiParam {Number} user User's unique ID.
+ * @apiParam {Number} intake Amount of water intake (in milliliters).
+ *
+ * @apiSuccess {Number} todaysIntake Updated or newly created water intake value (in milliliters).
+ *
+ * @apiSuccessExample {json} Success Response
+ * HTTP/1.1 200 OK
+ * {
+ *   "todaysIntake": 2000
+ * }
+ *
+ * @apiErrorExample {json} Error Response
+ * HTTP/1.1 500 Internal Server Error
+ * {
+ *   "error": "Internal Server Error"
+ * }
+ *
+ * @apiDescription This endpoint adds or updates the water intake record for a user on the current date.
+ * If a record for today already exists, the intake value is updated. If no record is found, a new record is created.
+ */
 //Post route for when there is no intake for today
 
 router.post("/", async (req, res) => {
@@ -64,6 +113,7 @@ router.post("/", async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
+
 
 // PUT route to add consumption to today's intake
 router.put("/", async (req, res) => {
